a17ba30cb1f9d3ff413677272900adac24e07353
owncook1 <chopgaming8@gmail.com>
2026-06-17 19:07:27 +0530

    feat: changed home to world

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,14 +1,16 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>🐸 Pepe Arcade</h1>
-
-      <ul>
-        <li>
-          <a href="/games/jet-frog">Jet Frog</a>
-          <a href="/world">Pepe World</a>
-        </li>
-      </ul>
-    </main>
-  );
-}
+// export default function Home() {
+//   return (
+//     <main>
+//       <h1>🐸 Pepe Arcade</h1>
+//
+//       <ul>
+//         <li>
+//           <a href="/games/jet-frog">Jet Frog</a>
+//           <a href="/world">Pepe World</a>
+//         </li>
+//       </ul>
+//     </main>
+//   );
+// }
+//
+export { default } from "./world";
